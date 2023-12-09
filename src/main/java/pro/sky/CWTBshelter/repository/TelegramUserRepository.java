876271c923d8deb717110050fbd759fe38182620
YuZu <yurkazubkov@yandex.ru>
2023-12-09 13:26:41 +0300

    Added repository annotation

--- src/main/java/pro/sky/CWTBshelter/repository/TelegramUserRepository.java
+++ src/main/java/pro/sky/CWTBshelter/repository/TelegramUserRepository.java
@@ -1,10 +1,12 @@
 package pro.sky.CWTBshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pro.sky.CWTBshelter.model.TelegramUser;
 
 import java.util.Optional;
 
+@Repository
 public interface TelegramUserRepository extends JpaRepository<TelegramUser, Long> {
     Optional<TelegramUser> findByChatId(Long chatId);
 }
