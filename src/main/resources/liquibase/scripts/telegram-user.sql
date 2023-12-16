18f5ab3f6bd2bf6112369d4b8880f833b170c9a7
qmargo23 <elen.asplund@gmail.com>
2023-12-17 01:12:16 +0300

    В БД внесены 4 кошки 4 собаки как исходные данные для дальнейшего манипулирования.

--- src/main/resources/liquibase/scripts/telegram-user.sql
+++ src/main/resources/liquibase/scripts/telegram-user.sql
@@ -8,7 +8,7 @@ CREATE TABLE telegram_user (
 )
 
 -- changeset qmargo:1
-CREATE TABLE "animal"
+CREATE TABLE animal
 (
     id                  BIGSERIAL           PRIMARY KEY,
     type_animal         varchar(255)        NOT NULL,
@@ -16,3 +16,14 @@ CREATE TABLE "animal"
     in_shelter          boolean,
     health              boolean
 )
+-- changeset qmargo:2
+INSERT INTO animal (type_animal,    breed,      in_shelter,         health)
+VALUES             ('dog',       'дворняжка',        true,           false),
+                   ('dog',       'овчарка',          false,          true),
+                   ('dog',       'пудель',           true,           true),
+                   ('dog',       'корги',            false,          true),
+
+                   ('cat',       'дворняжка',        true,           false),
+                   ('cat',       'сфинкс',           false,          true),
+                   ('cat',       'бенгал',           true,           true),
+                   ('cat',       'британец',         false,          true)
