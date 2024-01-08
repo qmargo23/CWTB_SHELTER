c7edd3b55d0c3ea3154d7a276144a4ee5d0ede51
qmargo23 <elen.asplund@gmail.com>
2024-01-08 16:18:58 +0300

    BotUpdatesListener стр33 добавила логирование , в методе init() прописаны 4 кнопки быстрого доступа, в методе process() будет ЛОГИКА БОТА - первичный фильтр, для дальнейших действий

--- src/main/java/pro/sky/CWTBshelter/listener/BotUpdatesListener.java
+++ src/main/java/pro/sky/CWTBshelter/listener/BotUpdatesListener.java
@@ -2,9 +2,17 @@ package pro.sky.CWTBshelter.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
+import com.pengrad.telegrambot.model.PhotoSize;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetFile;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import com.pengrad.telegrambot.response.GetFileResponse;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import pro.sky.CWTBshelter.dto.TelegramUserDTO;
 import pro.sky.CWTBshelter.dto.mapper.TelegramUserDTOMapper;
 import pro.sky.CWTBshelter.handler.Handler;
@@ -14,6 +22,7 @@ import pro.sky.CWTBshelter.model.BotState;
 import pro.sky.CWTBshelter.model.TelegramUser;
 import pro.sky.CWTBshelter.service.imp.TelegramUserServiceImpl;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +35,7 @@ public class BotUpdatesListener implements UpdatesListener {
     private final Map<BotState, Handler> handlerMap = new HashMap<>();
     private final BotState defaultBotState = BotState.START;
     private final TelegramUserDTOMapper telegramUserDTOMapper;
+    private final Logger logger = LoggerFactory.getLogger(BotUpdatesListener.class);
 
     public BotUpdatesListener(
             TelegramBot bot,
@@ -44,25 +54,50 @@ public class BotUpdatesListener implements UpdatesListener {
     @PostConstruct
     void init() {
         bot.setUpdatesListener(this);
+        bot.execute(new SetMyCommands(
+                new BotCommand("/start", " выбор приюта"),
+                new BotCommand("/adopt", " забрать питомца"),
+                new BotCommand("/report", " сдать отчет"),
+                new BotCommand("/help", "вызов волонтера")));
     }
 
     @Override
     public int process(List<Update> updates) {
-        for (Update update : updates) {
-            TelegramUser telegramUser = telegramUserService.getTelegramUserByChatId(update.message().chat().id());
-            if (telegramUser == null) {
-                telegramUser = telegramUserService.createTelegramUser(
-                        new TelegramUserDTO.Request.Create(update.message().chat().id(), defaultBotState, null)
-                ); // FIXME: заменить null на нового юзера
-            }
-            BotState botState = telegramUser.getBotState();
-            BotState nextBotState = handlerMap.get(botState).handle(update); // Обрабатываем обновление
-            if (!Objects.equals(nextBotState, botState)) { // Если состояние изменилось
-                handlerMap.get(nextBotState).makeTransition(update); // Делаем переход на следующее состояние
-                telegramUser.setBotState(nextBotState);
-                telegramUserService.updateTelegramUser(telegramUser.getId(), telegramUserDTOMapper.toCreateDTO(telegramUser));
-            }
+
+//        for (Update update : updates) {
+//            TelegramUser telegramUser = telegramUserService.getTelegramUserByChatId(update.message().chat().id());
+//            if (telegramUser == null) {
+//                telegramUser = telegramUserService.createTelegramUser(
+//                        new TelegramUserDTO.Request.Create(update.message().chat().id(), defaultBotState, null)
+//                ); // FIXME: заменить null на нового юзера
+//            }
+//            BotState botState = telegramUser.getBotState();
+//            BotState nextBotState = handlerMap.get(botState).handle(update); // Обрабатываем обновление
+//            if (!Objects.equals(nextBotState, botState)) { // Если состояние изменилось
+//                handlerMap.get(nextBotState).makeTransition(update); // Делаем переход на следующее состояние
+//                telegramUser.setBotState(nextBotState);
+//                telegramUserService.updateTelegramUser(telegramUser.getId(), telegramUserDTOMapper.toCreateDTO(telegramUser));
+//            }
+//        }
+
+        //____EL________EL________EL____
+        try {
+            updates.forEach(update -> {
+                logger.info("Handles update: {}", update);
+                if (update.callbackQuery() != null) {
+//вызвать методы класса  для обработки обратных запросов - при нажатии КНОПКИ
+                } else if (update.message().text() != null) {
+// вызвать методы класса обрабатывающие  update.message().text() - когда введен ТЕКСТ
+                } else if (update.message().photo() != null || update.message().caption() != null) {
+// вызвать методы класса обрабатывающие ФОТО, ВИДЕО иди ДОКУМЕНТЫ
+                }
+            });
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
+
+        //____EL________EL________EL____
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
