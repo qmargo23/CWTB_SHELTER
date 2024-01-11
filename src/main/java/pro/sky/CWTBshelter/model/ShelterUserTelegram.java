4eecdc9bb1203ff62e4ea8f8473754c5edc42aba
LuckyMirik <Luckymirik@mail.ru>
2024-01-11 18:05:42 +0300

    Немного документации к ButtonReactionService.java,HandlerService.java,ShelterUserTelegram.java,ShelterUserTelegramRepository.java

--- src/main/java/pro/sky/CWTBshelter/model/ShelterUserTelegram.java
+++ src/main/java/pro/sky/CWTBshelter/model/ShelterUserTelegram.java
@@ -5,6 +5,29 @@ import lombok.*;
 
 import java.time.LocalDate;
 
+/**
+ *класс ShelterUserTelegram - сущность описывающая пользователя телеграм-бота.
+ * Содержит геттеры, сеттеры, переопределены equals,
+ * hashCode и toString
+ * содержит следующие поля:
+ * <br>
+ * id - идентификатор пользователя телеграм-бота
+ * <br>
+ * chatId - идентификатор чата
+ * <br>
+ * botState - состояние телеграмм бота
+ * <br>
+ * userName - имя пользователя
+ * <br>
+ * userSurname - фамилия пользователя
+ * <br>
+ * userPhoneNumber - номер телефона пользователя
+ * <br>
+ * adoptDate - дата принятия
+ * <br>
+ * shelterUserType - тип пользователя бота
+ * animal - животное(имеет зависимость один к одному с {@link Animal})
+ */
 @Getter
 @Setter
 @ToString
--- src/main/java/pro/sky/CWTBshelter/util/ButtonReactionService.java
+++ src/main/java/pro/sky/CWTBshelter/util/ButtonReactionService.java
@@ -4,5 +4,10 @@ import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.request.SendMessage;
 
 public interface ButtonReactionService {
+    /**
+     * Выдает реакции на кнопки выбранные пользователем
+     * @param callbackQuery-объект {@link CallbackQuery}
+     * @return Сообщение пользователю
+     */
     SendMessage buttonReaction(CallbackQuery callbackQuery);
 }
